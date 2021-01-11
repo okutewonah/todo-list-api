6f3bd24a67a88037199b081e9025a21b034fa050
Okute Wonah <okutewonah@gmail.com>
2021-01-11 02:23:05 +0200

    initial create, collection types created

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ab17110e1443da4fea8d786810803f40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '428f472d-6590-4e84-9627-97af2f0cee19'
+};
